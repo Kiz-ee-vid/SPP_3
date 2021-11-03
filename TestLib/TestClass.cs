c0fb1d2810d34005ad84907093d8e03ef62b9e26
Ilya <ilyakizeev@gmail.com>
2021-11-03 14:20:51 +0300

    Tests

---
+++ TestLib/TestClass.cs
@@ -0,0 +1,24 @@
+﻿namespace TestLib
+{
+    public class TestClass
+    {
+        public string type = "Test";
+
+        public int CoolProperty { get; set; }
+
+        public bool One(int a, string b) => false;
+        public double Two(double c, double d) => c + d;
+    }
+}
+
+namespace TestingNamespace
+{
+    public static class Wow
+    {
+        public static void ExtensionMethod(string str)
+        {
+
+        }
+    }
+}
+
